c5f71c7cdff9aa8ee28379b133293f9aee9bb3a2
gitanikzad <60496494+gitanikzad@users.noreply.github.com>
2023-08-09 19:19:45 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,54 +1,10 @@
-from game_data import data
-from art import logo,vs
 import random
+name=input("Enter you name?")
+# list=["Ana", "Sara", 'Gita', "Arash"]
+list=[]
+list=name.split(",")
+# print(list)
+x=random.randint(0,(len(list)-1))
 
-score=0
-flag=True
-flag1=True
-
-while flag1:
-  a=random.choice(data)
-  b=random.choice(data)
-  if a['follower_count']==b['follower_count']:
-    b=random.choice(data)
-  print(logo)  
-  
-  print(f"Compare A: {a['name']},{a['description']},from {a['country']}")
-  print(vs)
-  print(f"Against B: {b['name']},{b['description']},from {b['country']}")
-  while flag:
-    input_followers=input("Who has more followers? Type 'A' or 'B':").lower()
-      
-    def find_win(a,b):
-      if a['follower_count']>b['follower_count']:
-      
-        return("a")
-      else:
-        return("b")
-      
-    
-    result=find_win(a,b)
-    
-    if input_followers==result:
-      
-      score+=1
-      print(f"Youre right! current score: {score}.")
-      
-      a=b
-      print(f"Compare A: {a['name']},{a['description']},from {a['country']}")
-      print(vs)
-      b=random.choice(data)
-      print(f"Against B: {b['name']},{b['description']},from {b['country']}")
-      
-    else:
-      
-      print(f"You lose current score: {score}.")  
-      answer=input("Do you want to continue? 'y' or 'n'.")
-      flag=False
-  if answer=='y'and flag1==True:
-      flag=True
-      score=0
-  else:
-      print(f"You'r total score is {score}.")
-      flag=False
-      flag1=False
+#print(list)
+print(f"{list[x]} should pay for lunch")
